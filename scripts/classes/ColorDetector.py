93205730d5b0bfe766b39f545e747abc53f5d5e1
Roger Truchero <truchero.roger@gmail.com>
2020-11-29 01:47:43 +0100

    Geometric canvas filtering

--- scripts/classes/ColorDetector.py
+++ scripts/classes/ColorDetector.py
@@ -34,32 +34,12 @@ class ColorDetector():
             "red" : ([0, 0, 50], [50, 50, 255])
         }
 
-
-    def identify_color_areas(self, image_path, color="green"):
-        """Identify regions between lower and upper color intervals.
-        Estimate the empty area using the color.
-        Based on this value send fill alarm.
-        The image also shows the capture with the empty areas within a rectangle.
-
-        Args:
-            image_path (string): Image path.
-            color (string, optional): Color to detect. Defaults to "green".
-
-        Returns:
-            string: the saved image path if all ok, empty string otherwise.
-        """
-        if color not in self.colors:
-            self.logger.error(":identify_area color: {0} error: Invalid color!".format(color), get_lineno())
-            return ""
-
-        lower, upper = np.array(self.colors[color], dtype="uint8") # Obtain corresponding lower and upper color values
-        image = cv2.imread(image_path) # Read image
-        mask = cv2.inRange(image, lower, upper) # Find the color specified and apply the mask
-        output = cv2.bitwise_and(image, image, mask=mask)
-        save_path = self.save_dir + image_path.split("/")[-1] # Define save path
-        cv2.imwrite(save_path, output) # Save image
-
-        return save_path
+        # Coordinates coefficients
+        self.B_COEFF = 0.6481
+        self.C_COEFF = 0.5338
+        self.D_COEFF_W = self.C_COEFF
+        self.D_COEFF_H = 0.3981
+        self.F_COEFF = 0.2344
 
 
     def identify_color_contours(self, image_path, color="green"):
@@ -84,19 +64,21 @@ class ColorDetector():
 
         lower, upper = np.array(self.colors[color], dtype="uint8") # Obtain corresponding lower and upper color values
         image = cv2.imread(image_path) # Read image
-        #hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) # Convert BGR to HSV
+        self.HEIGHT, self.WIDTH, _ = image.shape # Obtain height and widht sizes
+        self.logger.info(":identify_color_contours id: {0} height: {1} width: {2}".format(id, self.HEIGHT, self.WIDTH), get_lineno())
+
         mask = cv2.inRange(image, lower, upper) # Find the color specified and apply the mask
-        contours = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        contours = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2] # Find all contours
 
         if len(contours) > 0:
             #red_area = max(contours, key=cv2.contourArea)
             for contour in contours:
                 x, y, w, h = cv2.boundingRect(contour)
                 contour_area = cv2.contourArea(contour)
-                if self.is_valid_contour(contour_area):
-                    cv2.rectangle(image, (x, y),(x+w, y+h), (0, 0, 255), 2)
+                if self.is_valid_contour(contour_area, x, y, w, h):
+                    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 2)
                     #cv2.drawContours(image, contour, -1, (0, 0, 255), 2)
-                    self.logger.info(":identify_color_contours id: {0} contour_area: {1}".format(id, contour_area), get_lineno())
+                    self.logger.info(":identify_color_contours id: {0} contour_area: {1} values: {2}".format(id, contour_area, (x, y, w, h)), get_lineno())
 
             # Make request directory
             ext = image_path.split(".")[-1]
@@ -117,9 +99,127 @@ class ColorDetector():
         return ""
 
 
-    def is_valid_contour(self, contour_area):
-        # todo: Com sabem el valor de l'àrea necessari per notificar de la falta de stock?
-        return contour_area >= 300
+    def print_triangle_lines(self, image):
+        """Print triangle lines in the canvas.
+
+        Args:
+            image (object): cv2 image object.
+
+        Returns:
+            object: Modified image canvas object with lines addition.
+        """
+        A = (0, 0)
+        B = tuple(map(int, (0, self.HEIGHT*self.B_COEFF)))
+        C = tuple(map(int, (self.WIDTH*self.C_COEFF, 0)))
+        D = tuple(map(int, (self.WIDTH*self.D_COEFF_W, self.HEIGHT*self.D_COEFF_H)))
+        E = tuple(map(int, (self.WIDTH*self.C_COEFF, self.HEIGHT)))
+        F = tuple(map(int, (self.WIDTH*self.F_COEFF, self.HEIGHT)))
+
+        cv2.line(image, A, B, (0, 0, 255), 3) # Segment AB
+        cv2.line(image, B, C, (0, 0, 255), 3) # Segment BC
+        cv2.line(image, C, A, (0, 0, 255), 3) # Segment CA
+        cv2.line(image, D, E, (0, 0, 255), 3) # Segment DE
+        cv2.line(image, E, F, (0, 0, 255), 3) # Segment EF
+        cv2.line(image, F, D, (0, 0, 255), 3) # Segment FD
+        cv2.line(image, C, D, (0, 255, 0), 3) # Segment CD
+
+        return image
+
+
+    def is_valid_contour(self, contour_area, x, y, w, h):
+        """Check if contour is valid.
+
+        Args:
+            contour_area (float): effective colored area.
+            x (int): top-left rectangle x coordinate.
+            y (int): top-left rectangle y coordinate.
+            w (int): rectangle width.
+            h (int): rectangle height.
+
+        Returns:
+            boolean: True if point is within the valid area, False otherwise.
+        """
+        # Compute the ponderate point coordinates
+        A = (0, 0)
+        B = tuple(map(int, (0, self.HEIGHT*self.B_COEFF)))
+        C = tuple(map(int, (self.WIDTH*self.C_COEFF, 0)))
+        D = tuple(map(int, (self.WIDTH*self.D_COEFF_W, self.HEIGHT*self.D_COEFF_H)))
+        E = tuple(map(int, (self.WIDTH*self.C_COEFF, self.HEIGHT)))
+        F = tuple(map(int, (self.WIDTH*self.F_COEFF, self.HEIGHT)))
+        P = (x+w, y+h)
+        #self.logger.info(":is_valid_contour contour_area: {0} A: {1} B: {2} C: {3} D: {4} E: {5} F: {6} P: {7}".format(contour_area, A, B, C, D, E, F, P), get_lineno())
+
+        """
+            We only are interested in x parts so, we exclude the following vectorial points:
+                * Points within ABC triangle
+                * Points within DEF triangle
+                * Points at the right of C
+                * Points with an effective are greater than 300
+
+            A___________________C_______________________
+            |                xxxx                      |
+            |             xxxxxxx                      |
+            |        xxxxxxxxxxxD                      |
+            |     xxxxxxxxxxxx                         |
+            Bxxxxxxxxxxxxxx                            |
+            |xxxxxxxxxxxx                              |
+            |xxxxxxxxx                                 |
+            |xxxxxxxF___________E______________________|
+        """
+
+        return not self.is_inside(A[0], A[1], B[0], B[1], C[0], C[1], P[0], P[1]) and not self.is_inside(D[0], D[1], E[0], E[1], F[0], F[1], P[0], P[1]) and x <= C[0] and contour_area >= 300
+
+
+    # A utility function to calculate area of triangle formed by (x1, y1), (x2, y2) and (x3, y3)
+    def area(self, x1, y1, x2, y2, x3, y3):
+        """Calculates the are of a triangle using their vectorials coordinates.
+
+        Args:
+            x1 (int): vertex A.x triangle coordinate.
+            y1 (int): vertex A.y triangle coordinate.
+            x2 (int): vertex B.x triangle coordinate.
+            y2 (int): vertex B.y triangle coordinate.
+            x3 (int): vertex C.x triangle coordinate.
+            y3 (int): vertex C.y triangle coordinate.
+
+        Returns:
+            float: triangle area value.
+        """
+        return abs((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2.0)
+
+
+    # A function to check whether point P(x, y) lies inside the triangle formed by A(x1, y1), B(x2, y2) and C(x3, y3)
+    def is_inside(self, x1, y1, x2, y2, x3, y3, x, y):
+        """Check if a P(x, y) coordinate is inside ABC triangle.
+
+        Args:
+            x1 (int): vertex A.x triangle coordinate.
+            y1 (int): vertex A.y triangle coordinate.
+            x2 (int): vertex B.x triangle coordinate.
+            y2 (int): vertex B.y triangle coordinate.
+            x3 (int): vertex C.x triangle coordinate.
+            y3 (int): vertex C.y triangle coordinate.
+            x (int): vertex P.x triangle coordinate.
+            y (int): vertex P.y triangle coordinate.
+
+        Returns:
+            boolean: True if the coordinate P(x, y) is inside the ABC triangle, False otherwise.
+        """
+        # Calculate area of triangle ABC
+        A = self.area(x1, y1, x2, y2, x3, y3)
+
+        # Calculate area of triangle PBC
+        A1 = self.area(x, y, x2, y2, x3, y3)
+
+        # Calculate area of triangle PAC
+        A2 = self.area(x1, y1, x, y, x3, y3)
+
+        # Calculate area of triangle PAB
+        A3 = self.area(x1, y1, x2, y2, x, y)
+
+        # Check if sum of A1, A2 and A3 is same as A
+        return True if A == A1 + A2 + A3 else False
+
 
 
 if __name__ == "__main__":
